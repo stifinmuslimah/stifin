0678a6d927501632300db5a6ba5a2b0886e65b65
stifinmuslimah <infohijrahmind@gmail.com>
2025-09-28 20:56:54 +0700

    y

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import alpinejs from '@astrojs/alpinejs';
 
 import tailwindcss from '@tailwindcss/vite';
 
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel/static";
 
 // https://astro.build/config
 export default defineConfig({
