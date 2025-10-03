fb828cb6fc3d2f79aa15e46ee4666b9d6c4de4ea
stifinmuslimah <roy.yulis77@gmail.com>
2025-10-03 09:43:31 +0700

    siap push

--- src/lib/sanityClient.ts
+++ src/lib/sanityClient.ts
@@ -1,9 +1,9 @@
 import { createClient } from "@sanity/client";
 
 export const sanityClient = createClient({
-  projectId: "ui98z82j", // ganti sesuai yang di dashboard project baru
-  dataset: "production",
-  apiVersion: "2023-10-01",
+  projectId: process.env.SANITY_PROJECT_ID || "ui98z82j",
+  dataset: process.env.SANITY_DATASET || "production",
+  apiVersion: "2025-10-03",
   useCdn: false,
-  token: "sk8QOvCRJzulDj6CcnHZrhgjK6EM6T35QAZE2Nznjm2ti0L1ZBGMgCcBm9BOcdgjp6ADHMGluAUAU2pZ6QnAMoy0rzYskHYN929FTCCpLrewu7CP4LFVi03OtXR3IHSiEdkgfsXz6NsIXSre4rUFwi4rXvIfJhQy7ZSuDf7J0fQk4ngcDlvH" // ← tambahkan token baru
+  token: process.env.SANITY_AUTH_TOKEN, // ← jangan hardcode token di sini
 });
